de02ef8a4272933b47a0be4e0802b8bf9766df83
Aleksandr <139972964+sasha2155@users.noreply.github.com>
2023-10-31 14:36:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-open_weather_token = '0a4e67441e26b4ab8918e5bbfb3280fe'
-tg_bot_token = '6827074001:AAEDwAitlghHfCKJB3VpWh5N8q84A-Jc6Cw'
+open_weather_token = 'add a token'
+tg_bot_token = 'add a token'
